e05d4fdb822f8b34212740231d9edd8ea05015e7
na312046 <narendra.yadav4@wipro.com>
2015-11-18 14:26:34 +0530

    commit apps New

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,6 +10,9 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+            Console.Write("test apps");
+            Console.ReadKey();
         }
+
     }
 }
